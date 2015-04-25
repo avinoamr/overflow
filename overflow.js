b1836e6e5dfea621e4f6db5f87fd8e7836063458
Roi Avinoam <avinoamr@gmail.com>
2015-04-25 16:56:24 +0300

    Implement and test all of the functions

--- overflow.js
+++ overflow.js
@@ -1,12 +1,16 @@
 var stream = require( "stream" );
 var util = require( "util" );
 
-module.exports = OverflowStream;
-module.exports.Stream = OverflowStream;
+module.exports = Stream;
+module.exports.Stream = Stream;
 
-util.inherits( OverflowStream, stream.Duplex );
+util.inherits( Stream, stream.Duplex );
 
 function Stream () {
+    if ( !( this instanceof Stream ) ) {
+        return new Stream()
+    }
+
     var options = { objectMode: true, highWaterMark: 16 };
     stream.Duplex.call( this, options )
 
@@ -25,7 +29,7 @@ Stream.prototype._write = function ( chunk, encoding, callback ) {
     var ret = this.w.write.call( this.w, chunk, encoding, callback );
 
     // if the writable substream has ended, end this external stream as well
-    maybeEndWriter( this._writableState, this.w );
+    maybeEndWriter( this );
     return ret;
 }
 
@@ -39,23 +43,34 @@ Stream.prototype._read = function () {
     }
 }
 
+// end the underlying writer upon end
 Stream.prototype.end = function () {
     var ret = stream.Duplex.prototype.end.apply( this, arguments );
     maybeEndWriter( this );
     return ret;
 }
 
+// resize the internal buffers
+Stream.prototype.resize = function ( n ) {
+    [ this, this.w, this.r ].forEach( function ( s ) {
+        s._writableState.highWaterMark = 
+        s._readableState.highWaterMark = n;
+    })
+    return this;
+}
+
 Stream.prototype.through = 
-Stream.prototype.transform = function ( substream, flush ) {
+Stream.prototype.transform = 
+Stream.prototype.substream = function ( substream, flush ) {
     // in-line function constructs transform substreams
     if ( typeof substream == "function" ) {
         var transform = substream;
         substream = new stream.Transform({ objectMode: true, highWaterMark: 16 });
-        substream._transform = function ( chunk, encoding, done ) {
+        substream._transform = function ( data, encoding, done ) {
             return transform.call( this, data, done );
         }
 
-        if ( typeof flush == "functon" ) {
+        if ( typeof flush == "function" ) {
             substream._flush = flush;
         }
     }
@@ -71,6 +86,7 @@ Stream.prototype.transform = function ( substream, flush ) {
         .pipe( substream )
         .on( "error", this.emit.bind( this, "error" ) ) // propagate errors
         .pipe( this.r );
+
     this.emit( "through", substream );
     this.last = substream
     return this;
@@ -78,25 +94,25 @@ Stream.prototype.transform = function ( substream, flush ) {
 
 Stream.prototype.filter = function ( fn ) {
     return this.through( function ( data, done ) {
-        return fn.call( this, data, function ( keep ) {
-            return done( null, keep ? data, undefined );
+        return fn.call( this, data, function ( err, keep ) {
+            return done( err, keep ? data : undefined );
         })
     });
 }
 
 Stream.prototype.map = function ( fn ) {
     return this.through( function ( data, done ) {
-        return fn.call( this, data, function ( mapped ) {
-            return done( null, mapped );
+        return fn.call( this, data, function ( err, mapped ) {
+            return done( err, mapped );
         })
     })
 }
 
 Stream.prototype.reduce = function ( fn, memo ) {
     return this.through( function ( data, done ) {
-        return fn.call( this, memo, data, function ( _memo ) {
+        return fn.call( this, memo, data, function ( err, _memo ) {
             memo = _memo;
-            done();
+            done( err );
         })
     }, function ( done ) {
         if ( memo !== null ) {
@@ -106,13 +122,12 @@ Stream.prototype.reduce = function ( fn, memo ) {
     })
 }
 
-Stream.prototype.every = 
-Stream.prototype.all = function ( fn ) {
+Stream.prototype.every = function ( fn ) {
     var res = true;
     return this.through( function ( data, done ) {
-        return fn.call( this, data, function ( _res ) {
+        return fn.call( this, data, function ( err, _res ) {
             res = res && _res;
-            done();
+            done( err );
         })
     }, function ( done ) {
         this.push( res );
@@ -120,13 +135,12 @@ Stream.prototype.all = function ( fn ) {
     })
 }
 
-Stream.prototype.some =
-Stream.prototype.any = function ( fn ) {
+Stream.prototype.some = function ( fn ) {
     var res = false;
     return this.through( function ( data, done ) {
-        return fn.call( this, data, function ( _res ) {
+        return fn.call( this, data, function ( err, _res ) {
             res = res || _res;
-            done();
+            done( err );
         })
     }, function ( done ) {
         this.push( res );
@@ -134,11 +148,10 @@ Stream.prototype.any = function ( fn ) {
     })
 }
 
-Stream.prototype.each =
-Stream.prototype.forEach = function ( fn ) {
+Stream.prototype.each = function ( fn ) {
     return this.through( function ( data, done ) {
-        return fn.call( this, data, function () {
-            done( null, data );
+        return fn.call( this, data, function ( err ) {
+            done( err, data );
         })
     })
 }
@@ -147,18 +160,19 @@ Stream.prototype.slice = function ( begin, end ) {
     begin = begin || 0;
     end = end || Infinity;
     var i = 0;
-    return this.through( function ( data, done ) {
-        done( null, i >= begin || i < end ? data : undefined );
+    return this.filter( function ( data, done ) {
+        done( null, i >= begin && i < end );
         i += 1;
     })
 }
 
 
 
+
 function maybeEndWriter ( stream ) {
     var state = stream._writableState
     if ( state.ended && state.length <= 1 ) {
-        this.w.end();
+        stream.w.end();
     }
 }
 
---
+++ test.js
@@ -0,0 +1,278 @@
+var stream = require( "stream" );
+var assert = require( "assert" );
+var overflow = require( "./overflow" );
+
+describe( "Overflow", function () {
+
+    it( "passes through data by default", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ 1, 2, 3, 4, 5 ] );
+                done();
+            });
+    })
+
+    it( "adds substreams", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        double_ = new stream.Transform({ objectMode: true });
+        double_._transform = function ( chunk, encoding, cb ) {
+            cb( null, chunk * 2 );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .through( double_ )
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ 2, 4, 6, 8, 10 ] );
+                done();
+            });
+    })
+
+    it( "finish the underlying stream after .end()", function ( done ) {
+        function _done () {
+            if ( doubleFinished && currentFinished ) done();
+        }
+
+        var doubleFinished = currentFinished = false;
+        var written = [];
+        double_ = new stream.Transform({ objectMode: true });
+        double_._transform = function ( chunk, encoding, cb ) {
+            process.nextTick( function () {
+                written.push( chunk )
+                cb( null, chunk * 2 );
+            })
+        }
+
+        var s = overflow().through( double_ );
+
+        double_.on( "finish", function () {
+            assert.deepEqual( written, [ 1, 2, 3, 4, 5 ] );
+            doubleFinished = true;
+            _done()
+        })
+
+        s.on( "finish", function () {
+            currentFinished = true;
+            _done();
+        })
+
+
+        s.write( 1 );
+        s.write( 2 );
+        s.write( 3 );
+        s.write( 4 );
+        s.end( 5 );
+
+    });
+
+    it( "prevents substream overflow", function ( done ) {
+
+        // reader generates data endlessly, as long as someone is consuming
+        var i = 0;
+        var reader = new stream.Readable({ objectMode: true });
+        reader._read = function () {
+            // 10 is a rough limit instead of an exact match
+            // of the highWaterMark, due to internal substreams maintaining
+            // their own highWaterMark. This is not ideal but fixing it will
+            // require a huge increase in code complexity which is not
+            // a tradeoff i'm willing to make at this point.
+            assertLessThen( i, 100 );
+            this.push( i++ )
+        }
+
+        reader.pipe( overflow() ).resize( 1 ); 
+        // does not consume by current.read() or current.on( "data" )
+        // in an attempt to overflow the internal buffers.
+
+        setTimeout( done, 10 ); // 10ms went by without an assertion error
+    })
+
+    it( "propagate errors", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        reader
+            .pipe( overflow() )
+            .on( "error", function ( err ) {
+                assert.equal( err.message, "test" );
+                done();
+            })
+            .through( function ( d, done ) {
+                done( new Error( "test" ) )
+            })
+            .read();
+    })
+
+    it( ".filter()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .filter( function ( d, done ) {
+                done( null, d % 2 == 1 )
+            })
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ 1, 3, 5 ] )
+                done();
+            })
+    })
+
+    it( ".map()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .map( function ( d, done ) {
+                done( null, d * 2 );
+            })
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ 2, 4, 6, 8, 10 ] );
+                done();
+            })
+    })
+
+    it( ".reduce()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .reduce( function ( memo, d, done ) {
+                done( null, { 
+                    count: memo.count + 1, 
+                    sum: memo.sum + d
+                });
+            }, { count: 0, sum: 0 })
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [{ count: 5, sum: 15 }] )
+                done();
+            })
+    })
+
+    it( ".every()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .every( function ( d, done ) {
+                done( null, d > 2 );
+            })
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ false ] )
+                done();
+            })
+    })
+
+    it( ".some()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .some( function ( d, done ) {
+                done( null, d > 2 );
+            })
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ true ] )
+                done();
+            })
+    })
+
+    it( ".each()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var received = [];
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .each( function ( d, done ) {
+                received.push( d );
+                done();
+            })
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ 1, 2, 3, 4, 5 ] )
+                assert.deepEqual( received, [ 1, 2, 3, 4, 5 ] )
+                done();
+            })
+    })
+
+    it( ".slice()", function ( done ) {
+        var reader = new stream.Readable({ objectMode: true });
+        var data = [ 1, 2, 3, 4, 5 ];
+        reader._read = function () {
+            this.push( data.shift() || null );
+        }
+
+        var results = [];
+        reader
+            .pipe( overflow() )
+            .slice( 2, 4 )
+            .on( "data", results.push.bind( results ) )
+            .on( "end", function () {
+                assert.deepEqual( results, [ 3, 4 ] )
+                done();
+            })
+    })
+
+});
+
+function assertLessThen( small, big ) {
+    assert( small < big, small + " < " + big );
+}
+
+
+
+
+
